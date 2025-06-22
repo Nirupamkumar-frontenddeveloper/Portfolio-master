93aaecc2ed2d11286acd62644437bc746057421d
Nirupamkumar-frontenddeveloper <nirupamkumar2040@gmail.com>
2025-06-22 09:08:47 +0530

    d

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Nirupam Kumar </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Bihar, India.</span>
             <br />
             I am currently employed as a Frontend developer at Stock Archery.
             <br />
